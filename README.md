bb8102b0146e7043bc4682ecc67429f40fdc6f31
Jynxae <realkhalily@hotmail.com>
2023-08-19 17:13:12 -0500

    Tailwind CSS setup
    Set up tailwind CSS, Learning to manipulate items on page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Calculator
  Creating a simple calculator app with HTML, CSS, and Javascript
+This app will contain simple operations such as addition, multiplication, divison, and subtraction. 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],  //never include CSS files in this file.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
